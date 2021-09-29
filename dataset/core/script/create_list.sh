f22d879372660c7699afe1de5c788eee9d557160
杂杳 <2267379130@qq.com>
2021-09-29 14:26:17 +0800

    1.add script ./

---
+++ dataset/core/script/create_list.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+bash_dir="$(pwd)"
+root_dir=$bash_dir/../../../dataset/VOCdevkit2007/
+echo $root_dir
+sub_dir=ImageSets/Main
+for dataset in trainval test
+do
+  dst_file=$bash_dir/$dataset.txt
+  if [ -f $dst_file ]
+  then
+    rm -f $dst_file
+  fi
+  for name in VOC2007
+  do
+    echo "Create list for $name $dataset..."
+    dataset_file=$root_dir/$name/$sub_dir/$dataset.txt
+
+    img_file=$bash_dir/$dataset"_img.txt"
+    cp $dataset_file $img_file
+    sed -i "s/^/$name\/JPEGImages\//g" $img_file
+    sed -i "s/$/.jpg/g" $img_file
+
+    label_file=$bash_dir/$dataset"_label.txt"
+    cp $dataset_file $label_file
+    sed -i "s/^/$name\/Annotations\//g" $label_file
+    sed -i "s/$/.xml/g" $label_file
+
+    paste -d' ' $img_file $label_file >> $dst_file
+
+    rm -f $label_file
+    rm -f $img_file
+  done
+
+  # Generate image name and size infomation.
+  if [ $dataset == "test" ]
+  then
+    $bash_dir/../../../caffe/Build/x64/Debug/get_image_size $root_dir $dst_file $bash_dir/$dataset"_name_size.txt"
+  fi
+
+  # Shuffle trainval file.
+  if [ $dataset == "trainval" ]
+  then
+    rand_file=$dst_file.random
+    cat $dst_file | perl -MList::Util=shuffle -e 'print shuffle(<STDIN>);' > $rand_file
+    mv $rand_file $dst_file
+  fi
+done
