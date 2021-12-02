6c7c3bd7ae3c2677e830e6625cb618641512b0f4
杂杳 <2267379130@qq.com>
2021-12-02 14:48:58 +0800

    1.update statistics_dataset.py 新增修改label名称并优化

--- dataset/core/check/statistics_dataset.py
+++ dataset/core/check/statistics_dataset.py
@@ -7,8 +7,8 @@ import numpy as np
 def parse_obj(xml_path, filename):
     '''
     解析对象属性
-    :param xml_path:
-    :param filename:
+    :param xml_path: xml文件根路径
+    :param filename: xml文件名称
     :return:
     '''
     tree = ET.parse(xml_path + filename)
@@ -16,23 +16,36 @@ def parse_obj(xml_path, filename):
 
     for obj in tree.findall('object'):
         obj_struct = {}
-    obj_struct['name'] = obj.find('name').text
-    objects.append(obj_struct)
+        obj_struct['name'] = obj.find('name').text
+        objects.append(obj_struct)
     return objects
 
 
-def update_xml(xml_path, find_):
+def update_xml(root_name, element='object', search_name='name', result=''):
+    '''
+    读取xml并更新label名称name
+    :param root_name: xml文件根路径
+    :param element: 搜索元素名称
+    :param search_name: 名称
+    :param result:
+    :return:
+    '''
     # 查找节点并更新
-    root = ET.parse(xml_path)
-    for node in root.findall(".//folder"):
-        if node.text == "/home":
-            node.tag = "path"
-    ET.dump(root)  # 打印xml
-    root.write("output.xml")
+    root_dir = root_name + '.xml'
+    root = ET.parse(root_dir)
+    for node in root.findall(element):
+        name = node.find('name').text
+        # print('nname', name)
+        # print('node.text',node.text)
+        if name != search_name:
+            node.find('name').text = result
+            # print('result', result)
+            # node.tag = 'object'
+    # ET.dump(root)  #打印xml
+    root.write(root_dir)
 
-
-def Analysis_statistics_dataset(xml_path):
-    filenamess = os.listdir(xml_path)
+def Analysis_statistics_dataset(xml_root_dir):
+    filenamess = os.listdir(xml_root_dir)
     filenames = []
     print('filenamess', filenamess)
     print('len', len(filenamess))
@@ -46,7 +59,7 @@ def Analysis_statistics_dataset(xml_path):
             name = name.split('.xml')[0]
             # print('nname',name)
             filenames.append(name)
-    # print('ss', len(filenames))
+
     recs = {}
     obs_shape = {}
     classnames = []
@@ -56,9 +69,23 @@ def Analysis_statistics_dataset(xml_path):
     for i, nm in enumerate(filenames):
         try:
             # print('name', nm)
-            recs[nm] = parse_obj(xml_path, nm + '.xml')
+            recs[nm] = parse_obj(xml_root_dir, nm + '.xml')
         except:
             print('error')
+        # print('更新xml..')
+
+        # ---------------------------------------------------#
+        #   更新xml-object中label标签
+        # ---------------------------------------------------#
+        root_name = xml_root_dir + nm
+        element = 'object'
+        search_name = 'name'
+        result = 'insulator'
+        update_xml(root_name, element, search_name, result)
+
+    # ---------------------------------------------------#
+    #   根据keys进行object信息的整理统计
+    # ---------------------------------------------------#
     for name in filenames:
         # print('name', name)
         for object in recs[name]:
@@ -79,13 +106,13 @@ def Analysis_statistics_dataset(xml_path):
 
 
 if __name__ == '__main__':
-    # xml_path = '/media/hxzh02/SB@home/hxzh/Dataset/Plane_detect_datasets/VOCdevkit_tower_part/VOC2007/Annotations/'
-    # xml_path = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/7-输电线路绝缘子数据集VOC/dataset_insulator/VOC2007/Annotations/'
-    # xml_path = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/3-输电线路异物数据集（VOC）/foreignbody_dataset_part1/VOC2007/Annotations/'
-    # xml_path = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/1-火焰数据集/fire_dataset/VOC2007/Annotations/'
-    # xml_path = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/5-安全帽数据集5000张/dataset_safetyHat/VOC2007/Annotations/'
-    # xml_path = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/6-井盖电箱线杆标石头2400张/D0009/Annotations/'
-    # xml_path = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/8-输电线路金具VOC/2511bwb_5/Annotations/'
-    # xml_path = '/media/hxzh02/SB@home/hxzh/Dataset/无人机杆塔航拍数据集/杆塔主体/VOCdevkit_tower_part/Annotations/'
-    xml_path = '/media/hxzh02/TU100Pro/Insulator/train/voc labels/'
-    Analysis_statistics_dataset(xml_path=xml_path)
+    # xml_root_dir = '/media/hxzh02/SB@home/hxzh/Dataset/Plane_detect_datasets/VOCdevkit_tower_part/VOC2007/Annotations/'
+    # xml_root_dir = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/7-输电线路绝缘子数据集VOC/dataset_insulator/VOC2007/Annotations/'
+    # xml_root_dir = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/3-输电线路异物数据集（VOC）/foreignbody_dataset_part1/VOC2007/Annotations/'
+    # xml_root_dir = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/1-火焰数据集/fire_dataset/VOC2007/Annotations/'
+    # xml_root_dir = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/5-安全帽数据集5000张/dataset_safetyHat/VOC2007/Annotations/'
+    # xml_root_dir = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/6-井盖电箱线杆标石头2400张/D0009/Annotations/'
+    # xml_root_dir = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/8-输电线路金具VOC/2511bwb_5/Annotations/'
+    # xml_root_dir = '/media/hxzh02/SB@home/hxzh/Dataset/无人机杆塔航拍数据集/杆塔主体/VOCdevkit_tower_part/Annotations/'
+    xml_root_dir = '/media/hxzh02/TU100Pro/Insulator/train/voc labels/'
+    Analysis_statistics_dataset(xml_root_dir=xml_root_dir)
