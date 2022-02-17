92e48b0b25f1fcec18a7be3e568b1cf601c8be83
Xu Lin <2267379130@qq.com>
2022-02-17 23:22:04 +0800

    1.update downloadimage.py

--- dataset/core/network/downloadimage.py
+++ dataset/core/network/downloadimage.py
@@ -45,5 +45,35 @@ def download_image(key_word, save_name, download_max):
             continue
     print('下载完成')
 
+# 删除不是JPEG或者PNG格式的图片
+def delete_error_image(father_path):
+    # 获取父级目录的所有文件以及文件夹
+    try:
+        image_dirs = os.listdir(father_path)
+        for image_dir in image_dirs:
+            image_dir = os.path.join(father_path, image_dir)
+            # 如果是文件夹就继续获取文件夹中的图片
+            if os.path.isdir(image_dir):
+                images = os.listdir(image_dir)
+                for image in images:
+                    image = os.path.join(image_dir, image)
+                    try:
+                        # 获取图片的类型
+                        image_type = imghdr.what(image)
+                        # 如果图片格式不是JPEG同时也不是PNG就删除图片
+                        if image_type is not 'jpeg' and image_type is not 'png':
+                            os.remove(image)
+                            print('已删除：%s' % image)
+                            continue
+                        # 删除灰度图
+                        img = numpy.array(Image.open(image))
+                        if len(img.shape) is 2:
+                            os.remove(image)
+                            print('已删除：%s' % image)
+                    except:
+                        os.remove(image)
+                        print('已删除：%s' % image)
+    except:
+        pass
 
 
