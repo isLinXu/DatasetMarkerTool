79537e91b6c7cd94a49303eb02b5660aaa9875f4
杂杳 <2267379130@qq.com>
2022-04-01 14:52:40 +0800

    1.update yolo2coco.py

--- dataset/core/transform/yolo2coco.py
+++ dataset/core/transform/yolo2coco.py
@@ -1,102 +1,166 @@
-import json
+"""
+YOLO 格式的数据集转化为 COCO 格式的数据集
+--root_dir 输入根路径
+--save_path 保存文件的名字(没有random_split时使用)
+--random_split 有则会随机划分数据集，然后再分别保存为3个文件。
+--split_by_file 按照 ./train.txt ./val.txt ./test.txt 来对数据集进行划分。
+"""
+
+import os
 import cv2
+import json
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root_dir', default='./data',type=str, help="root path of images and labels, include ./images and ./labels and classes.txt")
+parser.add_argument('--save_path', type=str,default='./train.json', help="if not split the dataset, give a path to a json file")
+parser.add_argument('--random_split', action='store_true', help="random split the dataset, default ratio is 8:1:1")
+parser.add_argument('--split_by_file', action='store_true', help="define how to split the dataset, include ./train.txt ./val.txt ./test.txt ")
+
+arg = parser.parse_args()
+
+def train_test_val_split_random(img_paths,ratio_train=0.8,ratio_test=0.1,ratio_val=0.1):
+    # 这里可以修改数据集划分的比例。
+    assert int(ratio_train+ratio_test+ratio_val) == 1
+    train_img, middle_img = train_test_split(img_paths,test_size=1-ratio_train, random_state=233)
+    ratio=ratio_val/(1-ratio_train)
+    val_img, test_img  =train_test_split(middle_img,test_size=ratio, random_state=233)
+    print("NUMS of train:val:test = {}:{}:{}".format(len(train_img), len(val_img), len(test_img)))
+    return train_img, val_img, test_img
+
+def train_test_val_split_by_files(img_paths, root_dir):
+    # 根据文件 train.txt, val.txt, test.txt（里面写的都是对应集合的图片名字） 来定义训练集、验证集和测试集
+    phases = ['train', 'val', 'test']
+    img_split = []
+    for p in phases:
+        define_path = os.path.join(root_dir, f'{p}.txt')
+        print(f'Read {p} dataset definition from {define_path}')
+        assert os.path.exists(define_path)
+        with open(define_path, 'r') as f:
+            img_paths = f.readlines()
+            # img_paths = [os.path.split(img_path.strip())[1] for img_path in img_paths]  # NOTE 取消这句备注可以读取绝对地址。
+            img_split.append(img_paths)
+    return img_split[0], img_split[1], img_split[2]
 
 
-#第一个coco json类型
+def yolo2coco(arg):
+    root_path = arg.root_dir
+    print("Loading data from ",root_path)
 
-bnd_id_start = 1
+    assert os.path.exists(root_path)
+    originLabelsDir = os.path.join(root_path, 'labels')
+    originImagesDir = os.path.join(root_path, 'images')
+    with open(os.path.join(root_path, 'classes.txt')) as f:
+        classes = f.read().strip().split()
+    # images dir name
+    indexes = os.listdir(originImagesDir)
+    print('indexs', indexes)
 
-times = 0
+    if arg.random_split or arg.split_by_file:
+        # 用于保存所有数据的图片信息和标注信息
+        train_dataset = {'categories': [], 'annotations': [], 'images': []}
+        val_dataset = {'categories': [], 'annotations': [], 'images': []}
+        test_dataset = {'categories': [], 'annotations': [], 'images': []}
 
-json_dict = {
-    "images": [],
-    "type": "instances",
-    "annotations": [],
-    "categories" : []
-}
+        # 建立类别标签和数字id的对应关系, 类别id从0开始。
+        for i, cls in enumerate(classes, 0):
+            train_dataset['categories'].append({'id': i, 'name': cls, 'supercategory': 'mark'})
+            val_dataset['categories'].append({'id': i, 'name': cls, 'supercategory': 'mark'})
+            test_dataset['categories'].append({'id': i, 'name': cls, 'supercategory': 'mark'})
 
+        if arg.random_split:
+            print("spliting mode: random split")
+            train_img, val_img, test_img = train_test_val_split_random(indexes,0.8,0.1,0.1)
+        elif arg.split_by_file:
+            print("spliting mode: split by files")
+            train_img, val_img, test_img = train_test_val_split_by_files(indexes, root_path)
+    else:
+        dataset = {'categories': [], 'annotations': [], 'images': []}
+        for i, cls in enumerate(classes, 0):
+            dataset['categories'].append({'id': i, 'name': cls, 'supercategory': 'mark'})
 
+    # 标注的id
+    ann_id_cnt = 0
+    for k, index in enumerate(tqdm(indexes)):
+        # 支持 png jpg 格式的图片
+        txtFile = index.replace('images','txt').replace('.jpg','.txt').replace('.png','.txt')
+        # 读取图像的宽和高
+        im = cv2.imread(os.path.join(root_path, 'images/') + index)
+        print('path', os.path.join(root_path, 'images/') + index)
+        height, width, _ = im.shape
+        if arg.random_split or arg.split_by_file:
+            # 切换dataset的引用对象，从而划分数据集
+                if index in train_img:
+                    dataset = train_dataset
+                elif index in val_img:
+                    dataset = val_dataset
+                elif index in test_img:
+                    dataset = test_dataset
+        # 添加图像的信息
+        dataset['images'].append({'file_name': index,
+                                    'id': k,
+                                    'width': width,
+                                    'height': height})
+        if not os.path.exists(os.path.join(originLabelsDir, txtFile)):
+            # 如没标签，跳过，只保留图片信息。
+            continue
+        with open(os.path.join(originLabelsDir, txtFile), 'r') as fr:
+            labelList = fr.readlines()
+            for label in labelList:
+                label = label.strip().split()
+                x = float(label[1])
+                y = float(label[2])
+                w = float(label[3])
+                h = float(label[4])
 
+                # convert x,y,w,h to x1,y1,x2,y2
+                H, W, _ = im.shape
+                x1 = (x - w / 2) * W
+                y1 = (y - h / 2) * H
+                x2 = (x + w / 2) * W
+                y2 = (y + h / 2) * H
+                # 标签序号从0开始计算, coco2017数据集标号混乱，不管它了。
+                cls_id = int(label[0])
+                width = max(0, x2 - x1)
+                height = max(0, y2 - y1)
+                dataset['annotations'].append({
+                    'area': width * height,
+                    'bbox': [x1, y1, width, height],
+                    'category_id': cls_id,
+                    'id': ann_id_cnt,
+                    'image_id': k,
+                    'iscrowd': 0,
+                    # mask, 矩形是从左上角点按顺时针的四个顶点
+                    'segmentation': [[x1, y1, x2, y1, x2, y2, x1, y2]]
+                })
+                ann_id_cnt += 1
 
-# 这里是你的txt文件的读取
-with open('test_gt_TT100Kthree.txt','r') as f:
-    data = f.readlines()
+    # 保存结果
+    folder = os.path.join(root_path, 'annotations')
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    if arg.random_split or arg.split_by_file:
+        for phase in ['train','val','test']:
+            json_name = os.path.join(root_path, 'annotations/{}.json'.format(phase))
+            with open(json_name, 'w') as f:
+                if phase == 'train':
+                    json.dump(train_dataset, f)
+                elif phase == 'val':
+                    json.dump(val_dataset, f)
+                elif phase == 'test':
+                    json.dump(test_dataset, f)
+            print('Save annotation to {}'.format(json_name))
+    else:
+        json_name = os.path.join(root_path, 'annotations/{}'.format(arg.save_path))
+        with open(json_name, 'w') as f:
+            json.dump(dataset, f)
+            print('Save annotation to {}'.format(json_name))
 
-bnd_id = bnd_id_start
-
-
-for d in data:
-    content = d.split(";")
-    filename = content[0].split(".jpg")[0]     #这里可能修改，txt文件每一行第一个属性是图片路径，通过split()函数把图像名分离出来就行
-    # img = cv2.imread("/home/dell/桌面/GTSDB/VOC2007"+content[0])
-    try:
-        height, width=2048,2048
-        image_id = int(filename)
-    except:
-        times +=1
-        print('file is error')
-
-# type 已经填充
-
-#定义image 填充到images里面
-    image = {
-        'file_name' : filename,  #文件名
-        'height'    : height,    #图片的高
-        'width'     : width,     #图片的宽
-        'id'        : image_id   #图片的id，和图片名对应的
-    }
-    json_dict['images'].append(image)
-
-    c=content[1:]
-    xmin,ymin,xmax,ymax,label = c
-
-
-    xmin = int(xmin)
-    ymin = int(ymin)
-    xmax = int(xmax)
-    ymax = int(ymax)
-    o_width = abs(int(xmax) - int(xmin))
-    o_height = abs(int(ymax) - int(ymin))
-
-    area = o_width * o_height
-    category_id = label.strip()
-
-    # #定义annotationhb
-    annotation = {
-        'area'          : area,  #
-        'iscrowd'       : 0,
-        'image_id'      : image_id,  #图片的id
-        'bbox'          :[xmin, ymin, o_width,o_height],
-        'category_id'   : int(category_id)+1, #类别的id 通过这个id去查找category里面的name
-        'id'            : bnd_id,  #唯一id ,可以理解为一个框一个Id
-        'ignore'        : 0,
-        'segmentation'  : []
-    }
-
-    json_dict['annotations'].append(annotation)
-
-    bnd_id += 1
-    #
-#定义categories
-
-#你得类的名字(cid,cate)对应
-classes = ['0','1','2','3']
-
-for i in range(len(classes)):
-
-    cate = classes[i]
-    cid = i
-    category = {
-        'supercategory' : 'none',
-        'id'            : cid,  #类别的id ,一个索引，主键作用，和别的字段之间的桥梁
-        'name'          : cate  #类别的名字比如房子，船，汽车
-    }
-
-    json_dict['categories'].append(category)
-
-
-
-json_fp = open("TT100K_val.json",'w')
-json_str = json.dumps(json_dict)
-json_fp.write(json_str)
-json_fp.close()
+if __name__ == "__main__":
+    arg.root_dir = '/home/hxzh02/文档/detr_tutorial/coco128(复件)'
+    # arg.save_path = ''
+    arg.random_split = '8:1:1'
+    # arg.split_by_file = ''
+    yolo2coco(arg)
