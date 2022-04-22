d7e9371157f4f8c3fdd2b040b5e9c0def7002c1b
杂杳 <2267379130@qq.com>
2022-04-22 20:00:21 +0800

    1.update merge_xml.py

--- dataset/core/change/merge_xml.py
+++ dataset/core/change/merge_xml.py
@@ -1,106 +1,70 @@
-import os, os.path, sys
-import glob
-import argparse
+from xml.etree.ElementTree import ElementTree, Element, parse
+import xml.etree.ElementTree as ET
+import os
 import shutil
 
-from xml.etree import ElementTree as ET
-from xml.dom import minidom
-from shutil import copy
+# 格式化
+from dataset.core.check.check_files import check_dir_bool
 
 
-def prettify(CONTENT):
-    reparsed = minidom.parseString(CONTENT)
-    return '\n'.join([line for line in reparsed.toprettyxml(indent=' ' * 4).split('\n') if line.strip()])
+def __indent(elem, level=0):
+    i = "\n" + level * "\t"
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "\t"
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            __indent(elem, level + 1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
 
 
-def merge_xml(path_to_xml_1, list_path_to_xml_2, new_path):
-    # read tree 1
-    tree1 = ET.parse(path_to_xml_1)
-    root1 = tree1.getroot()
-    # read tree 2
-    i = 1
-    for path_to_xml_2 in list_path_to_xml_2:
-        tree2 = ET.parse(path_to_xml_2)
-        root2 = tree2.getroot()
-        for obj in root2.iter('object'):
-            # name.text = nn
-            pose = ET.Element('pose')
-            pose.text = obj[1].text
-            truncated = ET.Element('truncated')
-            truncated.text = obj[2].text
-            difficult = ET.Element('difficult')
-            difficult.text = obj[3].text
-            bnbbox = ET.Element('bnbbox')
-            xmin = ET.SubElement(bnbbox, 'xmin')
-            xmin.text = obj[4][0].text
-            ymin = ET.SubElement(bnbbox, 'ymin')
-            ymin.text = obj[4][1].text
-            xmax = ET.SubElement(bnbbox, 'xmax')
-            xmax.text = obj[4][2].text
-            ymax = ET.SubElement(bnbbox, 'ymax')
-            ymax.text = obj[4][3].text
-            root1.insert(6 + i, obj)
-            i += 1
+def merge_annotations(hole_path, arm_path, out_path):
+    '''
+    合并同名标注文件
+    :param hole_path: 标注文件路径1
+    :param arm_path:  目标标注文件路径2
+    :param out_path:  合并输出文件路径3 => 2->1
+    :return:
+    '''
 
-    xmlstr = prettify(ET.tostring(root1))
-    if not os.path.exists(new_path):
-        os.makedirs(new_path)
-    name = path_to_xml_1.split("\\")[-1]
-    with open(os.path.join(new_path, name), 'w') as f:
-        f.write(xmlstr)
+    if check_dir_bool(hole_path) and check_dir_bool(arm_path) and check_dir_bool(out_path):
+        for hole_xml in os.listdir(hole_path):
+            # 将同名xml合并
+            if os.path.exists(os.path.join(arm_path, hole_xml)):
+                print('fusing', hole_xml)
+                tree_hole = parse(os.path.join(hole_path, hole_xml))
+                root_hole = tree_hole.getroot()  # annotation
 
+                new_hole = tree_hole
 
-if __name__ == "__main__":
-    args = argparse.ArgumentParser()
-    args.add_argument("-f", "--folder", default="/media/hxzh02/Double/数据集/merge")
-    # args.add_argument("-v", "--videoname")
-    args = vars(args.parse_args())
-    # print(args["folder"])
-
-    list_dirs = os.listdir(args["folder"])
-    # print(list_dirs)
-    for d in list_dirs:
-        print('d', d)
-        if os.path.isdir(os.path.join(args["folder"], d)):
-            new_path = os.path.join(args["folder"], d, "merged")
-            if os.path.exists(new_path):
-                shutil.rmtree(new_path)
-                os.makedirs(new_path)
+                tree_arm = parse(os.path.join(arm_path, hole_xml))
+                root_arm = tree_arm.getroot()  # annotation
+                object = (tree_arm.findall('object'))
+                for i in range(len(object)):
+                    root_hole.append(object[i])
+                __indent(root_hole)
+                new_hole.write(os.path.join(out_path, hole_xml))
+            # 不同名xml复制
             else:
-                os.makedirs(new_path)
+                print('copying', hole_xml)
+                shutil.copy(os.path.join(hole_path, hole_xml), out_path)
 
-            ### ----------------- ###
-            dir_list = [x[0] for x in os.walk(os.path.join(args["folder"], d))]
-            print(dir_list)
-            merged_idx = dir_list.index(os.path.join(args["folder"], d, "merged"))
-            print('merged_idx', merged_idx)
-            dir_list.pop(merged_idx)
-            dir_list = dir_list[1:]
-            print('dir_list', dir_list)
-            # print("///")
-            traversed = []
-            for folder in dir_list:
-                # print(folder)
-                for f in glob.glob(str(folder) + "/*.xml"):
-                    print(f)
-                    shutil.copy(f, os.path.join(new_path, f.split('/')[-1]))
+            # 将不同名xml复制
+            for arm_xml in os.listdir(arm_path):
+                if not os.path.exists(os.path.join(out_path, arm_xml)):
+                    print('copying')
+                    shutil.copy(os.path.join(arm_path, arm_xml), out_path)
+    else:
+        print('path not exist')
 
-            for i in range(len(dir_list)):
-                # for j in range(i+1, len(dir_list)):
-                path_1 = dir_list[i]
-                list_path_2 = dir_list[i + 1:]
-                for f in glob.glob(str(path_1) + "/*.xml"):
-                    list_to_merge = []
-                    for l in list_path_2:
-                        if os.path.exists(os.path.join(l, f.split('/')[-1])):
-                            if not f.split('/')[-1] in traversed:
-                                f2 = os.path.join(l, f.split('/')[-1])
-                                list_to_merge.append(f2)
-                                # traversed.append(f.split('\\')[-1])
-                        else:
-                            if not f.split('/')[-1] in traversed:
-                                # traversed.append(f.split('\\')[-1])
-                                copy(f, os.path.join(new_path, f.split('/')[-1]))
-                    traversed.append(f.split('/')[-1])
-                    if len(list_to_merge) != 0:
-                        merge_xml(f, list_to_merge, new_path)
+
+if __name__ == '__main__':
+    hole_path = '/media/hxzh02/Double/数据集/merge/Annotations'
+    arm_path = '/media/hxzh02/Double/数据集/merge1/xml'
+    out_path = '/media/hxzh02/Double/数据集/merge/merge'
+    merge_annotations(hole_path, arm_path, out_path)
