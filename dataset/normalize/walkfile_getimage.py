16ed0726e7994afa4dea151cd78da9c25ad7159a
isLinXu <2267379130@qq.com>
2022-07-13 16:14:47 +0800

    1.fix and update walkfile_getimage.py

--- dataset/normalize/walkfile_getimage.py
+++ dataset/normalize/walkfile_getimage.py
@@ -11,7 +11,8 @@ import shutil
 
 from tqdm import tqdm
 
-def normalize_walkfile(path, new_path,control = 'copy'):
+
+def normalize_walkfile(path, new_path, control='copy'):
     # ---------------------------------------------------#
     #   循环遍历路径，复制路径下所有图片文件至输出路径
     # ---------------------------------------------------#
@@ -22,40 +23,37 @@ def normalize_walkfile(path, new_path,control = 'copy'):
             strlist = files[i].split('.')
             name = strlist[0]
             end_form = strlist[1]
-            print('root',root)
+            # print('root', root)
             fileslist = root.split('/')
-            print('fileslist',fileslist)
-            print(name, end_form)
+            # print('fileslist', fileslist)
+            fname = fileslist[-1].split('.')[0]
+            # print('fname:', fname)
+            # print(name, end_form)
             if (end_form == 'jpg' or end_form == 'JPG' or end_form == 'png'):
                 # 文件路径拼接
                 file_path = root + '/' + files[i]
-                new_file_path = new_path + '/' + files[i]
+                new_file_path = new_path + '/' + fname + '-' + files[i]
                 if control == 'copy':
                     # 复制
                     shutil.copy(file_path, new_file_path)
                 else:
                     # 移动
-                    shutil.move(file_path,new_file_path)
-                print(count, ':file_path', file_path)
+                    shutil.move(file_path, new_file_path)
+                print('count:', count, 'file_path:', file_path)
                 count += 1
 
 
 if __name__ == '__main__':
-
     # 原始数据路径(包含所有图片)
     # path = '/home/hxzh02/文档/deep-learning-for-image-processing/pytorch_classification/Test3_vggnet/data_set/flower_data/'
     # path = '/media/hxzh02/SB@home/hxzh/Dataset/PPLTA航拍输电线路数据集/data_original_size/'
-    path = '/home/linxu/Desktop/workclothes/'
-
+    path = '/home/linxu/Desktop/dlib-face-detection/detect_video/result/'
 
     # 整理输出路径(汇总所有文件目录下的路径)
     # new_path = '/home/hxzh02/文档/deep-learning-for-image-processing/pytorch_classification/Test3_vggnet/data_set/flower_data/'
     # new_path = '/media/hxzh02/SB@home/hxzh/Dataset/PPLTA航拍输电线路数据集/data_original_size/'
-    new_path = '/home/linxu/Desktop/All/'
+    new_path = '/home/linxu/Desktop/Variational-AutoEncoder-in-Pytorch/Lips/xunfei/'
     # tips:path最好不要与new_path有交叉
 
     # 调用函数
-    normalize_walkfile(path, new_path,control = 'copy')
-
-
-
+    normalize_walkfile(path, new_path, control='copy')
