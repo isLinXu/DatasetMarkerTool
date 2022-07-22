b6ae80bb1498e584a50b1cabd98120d3ed4e2951
isLinXu <2267379130@qq.com>
2022-07-22 10:12:14 +0800

    1.add classifier.py 2.update image_classify.py

--- dataset/core/classify/image_classify.py
+++ dataset/core/classify/image_classify.py
@@ -3,14 +3,14 @@ from tkinter.messagebox import showinfo
 import os
 from PIL import Image, ImageTk
 
-# dir_path = "/home/linxu/Desktop/coco128/coco128/images/train2017/"  # 文件夹路径
-# prefix = "marked"  # 文本标记过后的前缀，防止重复标记
-#
-# top = tk.Tk()
-# top.title("图片标记器")
-# width = 640
-# height = 480
-# top.geometry(f'{width}x{height}')
+dir_path = "/home/linxu/Desktop/coco128/coco128/images/train2017/"  # 文件夹路径
+prefix = "marked"  # 文本标记过后的前缀，防止重复标记
+
+top = tk.Tk()
+top.title("图片标记器")
+width = 640
+height = 480
+top.geometry(f'{width}x{height}')
 
 
 # 标记当前图片，并切换到下一个图片
@@ -33,25 +33,25 @@ def next_img(type):
     label_img.configure(image=imgs[index])
 
 
-# imgs = []  # 所有图片
-# img_file_names = []  # 所有图片的名称
-# all_files = os.listdir(dir_path)
-# for f_name in all_files:  # 遍历所有图片
-#     if f_name.startswith(prefix) or not f_name.endswith(".jpg"):
-#         continue  # 如果已经标注过了，下一个
-#     img_path = dir_path + "/" + f_name
-#     img = Image.open(img_path)  # 打开图片
-#     photo = ImageTk.PhotoImage(img)  # 用PIL模块的PhotoImage打开
-#     # 把图片和名称存储起来
-#     imgs.append(photo)
-#     img_file_names.append(f_name)
+imgs = []  # 所有图片
+img_file_names = []  # 所有图片的名称
+all_files = os.listdir(dir_path)
+for f_name in all_files:  # 遍历所有图片
+    if f_name.startswith(prefix) or not f_name.endswith(".jpg"):
+        continue  # 如果已经标注过了，下一个
+    img_path = dir_path + "/" + f_name
+    img = Image.open(img_path)  # 打开图片
+    photo = ImageTk.PhotoImage(img)  # 用PIL模块的PhotoImage打开
+    # 把图片和名称存储起来
+    imgs.append(photo)
+    img_file_names.append(f_name)
 
-# # 先展示第一个
-# index = 0
-# label_img = tk.Label(top, image=imgs[index])
-# label_img.place(x=20, y=20)
-#
-# class_names = ['空调', '烧烤', '海滩', '蝉', '赛龙舟', '电风扇', '雪糕', '泳衣', '西瓜', '粽子']
+# 先展示第一个
+index = 0
+label_img = tk.Label(top, image=imgs[index])
+label_img.place(x=20, y=20)
+
+class_names = ['空调', '烧烤', '海滩', '蝉', '赛龙舟', '电风扇', '雪糕', '泳衣', '西瓜', '粽子']
 
 
 # 按键的回调
@@ -62,39 +62,39 @@ def callback(event):
         next_img(str(c))
 
 
-# # 绑定键盘事件
-# frame = tk.Frame(top, width=20, height=20)
-# frame.bind("<Key>", callback)
-# frame.focus_set()
-# frame.pack()
-#
-# # 摆放数字
-# b_x = 160  # 基础X位置
-# b_y = -10  # 基础Y位置
-# d_l = 75  # 间距
-# button = tk.Button(top, text="1 " + class_names[1], command=lambda: next_img("1"))
-# button.place(x=b_x, y=b_y + 1 * d_l)
-# button = tk.Button(top, text="2 " + class_names[2], command=lambda: next_img("2"))
-# button.place(x=b_x, y=b_y + 2 * d_l)
-# button = tk.Button(top, text="3 " + class_names[3], command=lambda: next_img("3"))
-# button.place(x=b_x, y=b_y + 3 * d_l)
-# button = tk.Button(top, text="4 " + class_names[4], command=lambda: next_img("4"))
-# button.place(x=b_x, y=b_y + 4 * d_l)
-# button = tk.Button(top, text="5 " + class_names[5], command=lambda: next_img("5"))
-# button.place(x=b_x, y=b_y + 5 * d_l)
-#
-# button = tk.Button(top, text="6 " + class_names[6], command=lambda: next_img("6"))
-# button.place(x=b_x + d_l, y=b_y + 1 * d_l)
-# button = tk.Button(top, text="7 " + class_names[7], command=lambda: next_img("7"))
-# button.place(x=b_x + d_l, y=b_y + 2 * d_l)
-# button = tk.Button(top, text="8 " + class_names[8], command=lambda: next_img("8"))
-# button.place(x=b_x + d_l, y=b_y + 3 * d_l)
-# button = tk.Button(top, text="9 " + class_names[9], command=lambda: next_img("9"))
-# button.place(x=b_x + d_l, y=b_y + 4 * d_l)
-# button = tk.Button(top, text="0 " + class_names[0], command=lambda: next_img("0"))
-# button.place(x=b_x + d_l, y=b_y + 5 * d_l)
-#
-# top.mainloop()
+# 绑定键盘事件
+frame = tk.Frame(top, width=20, height=20)
+frame.bind("<Key>", callback)
+frame.focus_set()
+frame.pack()
+
+# 摆放数字
+b_x = 160  # 基础X位置
+b_y = -10  # 基础Y位置
+d_l = 75  # 间距
+button = tk.Button(top, text="1 " + class_names[1], command=lambda: next_img("1"))
+button.place(x=b_x, y=b_y + 1 * d_l)
+button = tk.Button(top, text="2 " + class_names[2], command=lambda: next_img("2"))
+button.place(x=b_x, y=b_y + 2 * d_l)
+button = tk.Button(top, text="3 " + class_names[3], command=lambda: next_img("3"))
+button.place(x=b_x, y=b_y + 3 * d_l)
+button = tk.Button(top, text="4 " + class_names[4], command=lambda: next_img("4"))
+button.place(x=b_x, y=b_y + 4 * d_l)
+button = tk.Button(top, text="5 " + class_names[5], command=lambda: next_img("5"))
+button.place(x=b_x, y=b_y + 5 * d_l)
+
+button = tk.Button(top, text="6 " + class_names[6], command=lambda: next_img("6"))
+button.place(x=b_x + d_l, y=b_y + 1 * d_l)
+button = tk.Button(top, text="7 " + class_names[7], command=lambda: next_img("7"))
+button.place(x=b_x + d_l, y=b_y + 2 * d_l)
+button = tk.Button(top, text="8 " + class_names[8], command=lambda: next_img("8"))
+button.place(x=b_x + d_l, y=b_y + 3 * d_l)
+button = tk.Button(top, text="9 " + class_names[9], command=lambda: next_img("9"))
+button.place(x=b_x + d_l, y=b_y + 4 * d_l)
+button = tk.Button(top, text="0 " + class_names[0], command=lambda: next_img("0"))
+button.place(x=b_x + d_l, y=b_y + 5 * d_l)
+
+top.mainloop()
 
 
 def image_classify_action(dir_path, prefix):
