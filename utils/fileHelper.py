d668c9d21bf74d465557a61e39643e530b0f166f
isLinXu <2267379130@qq.com>
2022-07-22 16:09:34 +0800

    1.update fileHelper.py add func:trans_endwith_lower_upper 2.update remove.py

--- utils/fileHelper.py
+++ utils/fileHelper.py
@@ -1,4 +1,8 @@
 import os
+import os.path
+import glob
+
+
 def os_mkdir(path):
     '''
     目录创建/操作函数
@@ -22,3 +26,27 @@ def os_mkdir(path):
         # 如果目录存在则不创建，并提示目录已存在
         print(path + ' 目录已存在')
         return False
+
+
+def trans_endwith_lower_upper(realpath,mode='lower'):
+    # 将图片jpg文件后缀小写或者大写
+    extension = 'JPG'
+    file_list = glob.glob(realpath + '*.' + extension)
+    for root, dirs, files in os.walk(realpath):
+        print(root, dirs, files)
+        for file in files:
+            fname = file.split('.')[0]
+            fend = file.split('.')[1]
+            if mode == 'lower':
+                fend = fend.lower()
+            elif mode == 'upper':
+                fend = fend.upper()
+            print('fend', fend)
+
+            file_path = os.path.join(root, file)
+            trans_path = root + fname + '.' + fend
+            os.rename(file_path, trans_path)
+
+if __name__ == '__main__':
+    realpath = '/home/linxu/Desktop/JPG/'
+    trans_endwith_lower_upper(realpath, mode='upper')
