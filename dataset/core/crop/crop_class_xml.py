65d437989d6b440d5b041c1de3eb5828fe2491dd
isLinXu <2267379130@qq.com>
2022-08-16 16:45:01 +0800

    1.uddate crop_class_xml.py

--- dataset/core/crop/crop_class_xml.py
+++ dataset/core/crop/crop_class_xml.py
@@ -10,45 +10,46 @@ import cv2
 import os
 import xml.etree.ElementTree as ET
 
-# xml_file_dir = "./xmls"
-# img_file_dir = "./imgs"
-img_file_dir = '/home/linxu/Desktop/Datasets/配电站房数据集/image'
-xml_file_dir = '/home/linxu/Desktop/Datasets/配电站房数据集/xml'
-crop_img_dir = '/home/linxu/Desktop/Datasets/配电站房数据集/crop'
+def crop_for_classify(img_file_dir, xml_file_dir, crop_img_dir):
+    xmls = os.listdir(xml_file_dir)
+    for xml in xmls:
+        # 解析xml
+        tree = ET.parse(os.path.join(xml_file_dir,xml))
+        root = tree.getroot()
+
+        img_name = root.find("filename").text
+        img = cv2.imread(os.path.join(img_file_dir,img_name))
+
+        objects = root.findall('object')
+        for i, obj in enumerate(objects):
+            id = 0
+            label = obj.find('name').text
+            if label == ":":
+                label = "colon"
+
+            bb = obj.find('bndbox')
+            xmin = bb.find('xmin').text
+            ymin = bb.find('ymin').text
+            xmax = bb.find('xmax').text
+            ymax = bb.find('ymax').text
+
+            # 保存crop img
+            crop_img = img[int(ymin):int(ymax), int(xmin):int(xmax)]
+
+            # crop_img_name的命名方式：img_name+"_"+label+"_"+i
+            crop_img_name = os.path.splitext(img_name)[0] + "_" + label+"_"+str(i)+".jpg"
+
+            crop_img_save_dir = os.path.join(crop_img_dir,label)
+            # crop_img_save_dir =
+            print(crop_img_save_dir)
+            if os.path.exists(crop_img_save_dir) == 0:
+                os.makedirs(crop_img_save_dir)
+            crop_img_save_path = os.path.join(crop_img_save_dir, crop_img_name)
+            cv2.imwrite(crop_img_save_path,crop_img)
 
 
-xmls = os.listdir(xml_file_dir)
-for xml in xmls:
-    # 解析xml
-    tree = ET.parse(os.path.join(xml_file_dir,xml))
-    root = tree.getroot()
-
-    img_name = root.find("filename").text
-    img = cv2.imread(os.path.join(img_file_dir,img_name))
-
-    objects = root.findall('object')
-    for i, obj in enumerate(objects):
-        id = 0
-        label = obj.find('name').text
-        if label == ":":
-            label = "colon"
-
-        bb = obj.find('bndbox')
-        xmin = bb.find('xmin').text
-        ymin = bb.find('ymin').text
-        xmax = bb.find('xmax').text
-        ymax = bb.find('ymax').text
-
-        # 保存crop img
-        crop_img = img[int(ymin):int(ymax), int(xmin):int(xmax)]
-
-        # crop_img_name的命名方式：img_name+"_"+label+"_"+i
-        crop_img_name = os.path.splitext(img_name)[0] + "_" + label+"_"+str(i)+".jpg"
-
-        crop_img_save_dir = os.path.join(crop_img_dir,label)
-        # crop_img_save_dir =
-        print(crop_img_save_dir)
-        if os.path.exists(crop_img_save_dir) == 0:
-            os.makedirs(crop_img_save_dir)
-        crop_img_save_path = os.path.join(crop_img_save_dir, crop_img_name)
-        cv2.imwrite(crop_img_save_path,crop_img)
+if __name__ == '__main__':
+    img_file_dir = '/home/linxu/Desktop/Datasets/配电站房/JPEGImages'
+    xml_file_dir = '/home/linxu/Desktop/Datasets/配电站房/Annotations'
+    crop_img_dir = '/home/linxu/Desktop/Datasets/配电站房/crop_img'
+    crop_for_classify(img_file_dir, xml_file_dir, crop_img_dir)
