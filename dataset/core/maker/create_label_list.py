2fd01986eea5112b9702b741ad1a5f8d20beb1ad
杂杳 <2267379130@qq.com>
2022-08-24 09:22:10 +0800

    1.update create_label_list.py

--- dataset/core/maker/create_label_list.py
+++ dataset/core/maker/create_label_list.py
@@ -2,6 +2,8 @@ import os
 import random
 import shutil
 
+from tqdm import tqdm
+
 
 def create_label_list(xml_dir, img_dir, train_dir, train_img_dir,
                       val_dir, val_img_dir,
@@ -24,7 +26,9 @@ def create_label_list(xml_dir, img_dir, train_dir, train_img_dir,
         os.mkdir(train_img_dir)
         os.mkdir(val_img_dir)
 
-    for i, content in enumerate(path_list):
+    # for i, content in enumerate(path_list):
+    for i in tqdm(range(0, len(path_list))):
+        content = path_list[i]
         img, xml = content
         print('img:', img, 'xml:', xml)
         text = img + ' ' + xml + '\n'
@@ -45,14 +49,15 @@ def create_label_list(xml_dir, img_dir, train_dir, train_img_dir,
 
 
 if __name__ == '__main__':
-    xml_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection/dataset/voc128/Annotations'  # 标签文件地址
-    img_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection/dataset/voc128/JPEGImages'  # 图像文件地址
-    train_xml_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection/dataset/voc128/train.txt'
-    val_xml_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection/dataset/voc128/val.txt'
-    train_img_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection/dataset/voc128/train'
-    val_img_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection/dataset/voc128/val'
+    root_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection_YOLOSeries/dataset/COCO_PeiDian/'
+    xml_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection_YOLOSeries/dataset/COCO_PeiDian/Annotations'  # 标签文件地址
+    img_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection_YOLOSeries/dataset/COCO_PeiDian/JPEGImages'  # 图像文件地址
+    train_xml_dir = root_dir + 'train.txt'
+    val_xml_dir = root_dir + '/val.txt'
+    train_img_dir = root_dir + 'train'
+    val_img_dir = root_dir + 'val'
     ratio = 0.9
-    label_dir = '/media/hxzh02/SB@home/hxzh/PaddleDetection/dataset/voc128/label_list.txt'
+    label_dir = root_dir + 'label_list.txt'
 
     # label = ['insulator']  #设置你想检测的类别,生成标签文档
     # label = ['0_0_0_20_0_0', '0_0_0_16_0_0', '1_0_6_21_42_0', '1_0_0_1_8_1',
@@ -69,17 +74,19 @@ if __name__ == '__main__':
     #                '0_0_0_16_0_0', '0_0_0_28_0_0', '1_0_0_2_30_0', '1_0_6_21_42_0', '1_0_3_22_41_0',
     #                '1_0_3_22_46_0', '0_0_0_30_4_0', '1_0_3_22_45_0', '1_0_0_1_6_0', '1_0_0_1_8_0',
     #                '1_0_0_1_53_0', '1_0_3_22_47_0', '1_0_0_30_3_0', '1_0_6_21_43_0']
-
-    label = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-             'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-             'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-             'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
-             'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-             'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-             'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
-             'cell phone',
-             'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
-             'hair drier', 'toothbrush']
+    label = ['1_0_0_1_1_0', '0_0_0_28_0_0', '0_0_0_20_0_0',
+             '0_0_0_30_3_0', '0_0_0_30_2_0', '0_0_0_40_1_0', '0_0_0_30_7_0',
+             '0_0_0_30_4_0']
+    # label = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+    #          'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    #          'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+    #          'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
+    #          'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+    #          'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+    #          'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+    #          'cell phone',
+    #          'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
+    #          'hair drier', 'toothbrush']
     create_label_list(xml_dir, img_dir, train_xml_dir, train_img_dir,
                       val_xml_dir, val_img_dir,
                       label, ratio, label_dir)
