7b76b79e7ba37f54596bf6b5a227ad8d7982f8d1
isLinXu <2267379130@qq.com>
2023-10-08 18:07:46 +0800

    [dataset][core][extract] fix extract_coco_category.py

--- dataset/core/extract/extract_coco_category.py
+++ dataset/core/extract/extract_coco_category.py
@@ -5,7 +5,7 @@ import shutil
 import cv2
 from tqdm import tqdm
 
-def extract_coco_data(coco_file, output_dir, category_dict):
+def extract_coco_data(coco_file, img_file, output_dir, category_dict):
     # 读取COCO数据集文件
     with open(coco_file, 'r') as f:
         coco_data = json.load(f)
@@ -29,16 +29,26 @@ def extract_coco_data(coco_file, output_dir, category_dict):
 
         # 如果图像中包含指定的类别，则复制图像和标注数据到输出目录
         if has_category:
-            image_file = os.path.join(coco_data['images_dir'], image['file_name'])
-            output_image_dir = os.path.join(output_dir, 'images', 'train' if image['split'] == 'train' else 'val')
-            output_image_file = os.path.join(output_image_dir, image['file_name'])
+            image_file = os.path.join(img_file, image['file_name'] + '.jpg')
+            if 'train' in coco_file:
+                output_image_dir = os.path.join(output_dir, 'images', 'train')
+                output_label_dir = os.path.join(output_dir, 'labels', 'train')
+            elif 'val' in coco_file:
+                output_image_dir = os.path.join(output_dir, 'images', 'val')
+                output_label_dir = os.path.join(output_dir, 'labels', 'val')
+            # image_file = os.path.join(coco_data['images_dir'], image['file_name'])
+            # output_image_dir = os.path.join(output_dir, 'images', 'train' if image['split'] == 'train' else 'val')
+            # output_image_dir = os.path.join(output_dir, 'images', 'train')
+            output_image_file = os.path.join(output_image_dir, image['file_name'] + '.jpg')
+            print("output_image_file:", output_image_file)
             shutil.copy(image_file, output_image_file)
 
             # 读取图片文件并获取其宽度和高度
             img = cv2.imread(image_file)
             img_height, img_width, _ = img.shape
 
-            output_label_dir = os.path.join(output_dir, 'labels', 'train' if image['split'] == 'train' else 'val')
+            # output_label_dir = os.path.join(output_dir, 'labels', 'train')
+            # output_label_dir = os.path.join(output_dir, 'labels', 'train' if image['split'] == 'train' else 'val')
             output_label_file = os.path.join(output_label_dir, os.path.splitext(image['file_name'])[0] + '.txt')
             with open(output_label_file, 'w') as f:
                 for annotation in coco_data['annotations']:
@@ -61,7 +71,8 @@ def extract_coco_data(coco_file, output_dir, category_dict):
     print('数据集提取完成！')
 
 # 测试代码
-coco_file = 'path/to/coco.json'
-output_dir = 'path/to/output'
+coco_file = 'coco/annotations/instances_train2017.json'
+img_file = 'coco/train2017/'
+output_dir = 'datasets/search_dataset/coco'
 category_dict = {'person': 0, 'car': 1, 'bus': 2, 'truck': 3}  # 指定要抽取的类别和对应的类别索引
-extract_coco_data(coco_file, output_dir, category_dict)
+extract_coco_data(coco_file, img_file, output_dir, category_dict)
