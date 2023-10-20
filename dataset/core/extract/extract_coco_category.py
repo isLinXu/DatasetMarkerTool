cad08133f331cd3248dc1d96e88bb7b083faf515
isLinXu <2267379130@qq.com>
2023-10-20 16:15:56 +0800

    [dataset][core][extract] update extract_coco_category.py

--- dataset/core/extract/extract_coco_category.py
+++ dataset/core/extract/extract_coco_category.py
@@ -1,6 +1,8 @@
 import json
 import os
 import shutil
+
+import cv2
 from tqdm import tqdm
 
 '''
@@ -86,6 +88,8 @@ coco_classes:
   78: hair drier
   79: toothbrush
 '''
+
+
 def extract_coco_data(coco_file, coco_img_dir, output_dir, category_dict):
     # 读取COCO数据集文件
     with open(coco_file, 'r') as f:
@@ -103,67 +107,130 @@ def extract_coco_data(coco_file, coco_img_dir, output_dir, category_dict):
         # 判断图像中是否包含指定的类别
         has_category = False
         for annotation in coco_data['annotations']:
-            try:
-                if annotation['image_id'] == image['id']:
-                    if coco_data['categories'][annotation['category_id']]['name'] in category_dict.keys():
+            # try:
+            if annotation['image_id'] == image['id']:
+                try:
+                    # print("annotation:", annotation)
+                    category_name = coco_data['categories'][annotation['category_id']]['name']
+                    if category_name in category_dict.keys():
+                        print("annotation:", annotation)
+                        # print("image['id']:", image['id'])
                         has_category = True
-                        print(coco_data['categories'][annotation['category_id']]['name'])
-                        break
-            except:
-                continue
+                        print("category_name:{}".format(category_name), "category_dict.keys():", category_dict.keys())
+                        # 如果图像中包含指定的类别，则复制图像和标注数据到输出目录
+                        dir_name = 'train'
+                        images_dir = coco_img_dir + '/train2017'
+                        # if coco_file.find('train') != -1:
+                        #     dir_name = 'train'
+                        #     images_dir = coco_img_dir + '/train2017'
+                        # else:
+                        #     dir_name = 'val'
+                        #     images_dir = coco_img_dir + '/val2017'
+                        image_file = os.path.join(images_dir, image['file_name'])
+                        output_image_dir = os.path.join(output_dir, 'images', dir_name)
+                        output_image_file = os.path.join(output_image_dir, image['file_name'])
+                        print("image['id']:", image['id'])
+                        print("image_file:{}".format(image_file))
+                        print("output_image_file:{}".format(output_image_file))
 
-        # 如果图像中包含指定的类别，则复制图像和标注数据到输出目录
-        if has_category:
-            dir_name = 'train'
-            images_dir = coco_img_dir + '/train2017'
-            # if coco_file.find('train') != -1:
-            #     dir_name = 'train'
-            #     images_dir = coco_img_dir + '/train2017'
-            # else:
-            #     dir_name = 'val'
-            #     images_dir = coco_img_dir + '/val2017'
-            image_file = os.path.join(images_dir, image['file_name'])
-            output_image_dir = os.path.join(output_dir, 'images', dir_name)
-            output_image_file = os.path.join(output_image_dir, image['file_name'])
-            print("image_file:{}".format(image_file))
-            print("output_image_file:{}".format(output_image_file))
-            if not os.path.exists(output_image_dir):
-                os.makedirs(output_image_dir)
-            if not os.path.exists(output_image_file):
-                shutil.copy(image_file, output_image_file)
-            output_label_dir = os.path.join(output_dir, 'labels', dir_name)
-            output_label_file = os.path.join(output_label_dir, os.path.splitext(image['file_name'])[0] + '.txt')
-            print("output_label_file:{}".format(output_label_file))
-            print("output_label_dir:{}".format(output_label_dir))
-            if not os.path.exists(output_label_dir):
-                os.makedirs(output_label_dir)
-            if not os.path.exists(output_label_file):
-                with open(output_label_file, 'w') as f:
-                    for annotation in coco_data['annotations']:
-                        if annotation['image_id'] == image['id']:
-                            try:
-                                category = coco_data['categories'][annotation['category_id']]['name']
-                                if category in category_dict.keys():
-                                    category_index = category_dict[category]
-                                    bbox = annotation['bbox']
-                                    x_center = bbox[0] + bbox[2] / 2
-                                    y_center = bbox[1] + bbox[3] / 2
-                                    width = bbox[2]
-                                    height = bbox[3]
-                                    f.write('{0} {1:.6f} {2:.6f} {3:.6f} {4:.6f}\n'.format(category_index, x_center, y_center, width, height))
-                            except:
-                                continue
+                        # 读取图片文件并获取其宽度和高度
+                        img = cv2.imread(image_file)
+                        img_height, img_width, _ = img.shape
+
+                        if not os.path.exists(output_image_dir):
+                            os.makedirs(output_image_dir)
+                        if not os.path.exists(output_image_file):
+                            shutil.copy(image_file, output_image_file)
+                        output_label_dir = os.path.join(output_dir, 'labels', dir_name)
+                        output_label_file = os.path.join(output_label_dir,
+                                                         os.path.splitext(image['file_name'])[0] + '.txt')
+                        print("output_label_file:{}".format(output_label_file))
+                        print("output_label_dir:{}".format(output_label_dir))
+                        if not os.path.exists(output_label_dir):
+                            os.makedirs(output_label_dir)
+                        if not os.path.exists(output_label_file):
+                            with open(output_label_file, 'w') as f:
+                                for annotation in coco_data['annotations']:
+                                    if annotation['image_id'] == image['id']:
+                                        try:
+                                            # category = coco_data['categories'][annotation['category_id']]['name']
+                                            if category_name in category_dict.keys():
+                                                category_index = category_dict[category_name]
+                                                bbox = annotation['bbox']
+                                                x_center = bbox[0] + bbox[2] / 2
+                                                y_center = bbox[1] + bbox[3] / 2
+                                                width = bbox[2]
+                                                height = bbox[3]
+                                                # 归一化yolo格式
+                                                x = float(x_center) / img_width
+                                                y = float(y_center) / img_height
+                                                w = float(width) / img_width
+                                                h = float(height) / img_height
+                                                f.write(
+                                                    '{0} {1:.6f} {2:.6f} {3:.6f} {4:.6f}\n'.format(category_index, x, y,
+                                                                                                   w, h))
+                                        except Exception as e:
+                                            print("e:{}".format(e))
+                                            continue
+                except Exception as e:
+                    print("e:{}".format(e))
+                    continue
+    # except:
+    #     continue
+
+    # 如果图像中包含指定的类别，则复制图像和标注数据到输出目录
+    # if has_category:
+    # dir_name = 'train'
+    # images_dir = coco_img_dir + '/train2017'
+    # # if coco_file.find('train') != -1:
+    # #     dir_name = 'train'
+    # #     images_dir = coco_img_dir + '/train2017'
+    # # else:
+    # #     dir_name = 'val'
+    # #     images_dir = coco_img_dir + '/val2017'
+    # image_file = os.path.join(images_dir, image['file_name'])
+    # output_image_dir = os.path.join(output_dir, 'images', dir_name)
+    # output_image_file = os.path.join(output_image_dir, image['file_name'])
+    # print("image_file:{}".format(image_file))
+    # print("output_image_file:{}".format(output_image_file))
+    # if not os.path.exists(output_image_dir):
+    #     os.makedirs(output_image_dir)
+    # if not os.path.exists(output_image_file):
+    #     shutil.copy(image_file, output_image_file)
+    # output_label_dir = os.path.join(output_dir, 'labels', dir_name)
+    # output_label_file = os.path.join(output_label_dir, os.path.splitext(image['file_name'])[0] + '.txt')
+    # print("output_label_file:{}".format(output_label_file))
+    # print("output_label_dir:{}".format(output_label_dir))
+    # if not os.path.exists(output_label_dir):
+    #     os.makedirs(output_label_dir)
+    # if not os.path.exists(output_label_file):
+    #     with open(output_label_file, 'w') as f:
+    #         for annotation in coco_data['annotations']:
+    #             if annotation['image_id'] == image['id']:
+    #                 try:
+    #                     category = coco_data['categories'][annotation['category_id']]['name']
+    #                     if category in category_dict.keys():
+    #                         category_index = category_dict[category]
+    #                         bbox = annotation['bbox']
+    #                         x_center = bbox[0] + bbox[2] / 2
+    #                         y_center = bbox[1] + bbox[3] / 2
+    #                         width = bbox[2]
+    #                         height = bbox[3]
+    #                         f.write('{0} {1:.6f} {2:.6f} {3:.6f} {4:.6f}\n'.format(category_index, x_center, y_center, width, height))
+    #                 except:
+    #                     continue
+
+
     print('数据集提取完成！')
-
-
 
 # 测试代码
 # /media/linxu/LXWorkShop1/Dataset/coco数据集
 coco_json_file = '/media/linxu/LXWorkShop1/Dataset/coco数据集/annotations_trainval2017/annotations/instances_train2017.json'
 coco_img_dir = '/media/linxu/LXWorkShop1/Dataset'
 # output_dir = '/media/linxu/LXWorkShop1/Dataset/coco数据集/COCO_YOLO2017'
-output_dir = '/media/linxu/MobilePan/2-Data/COCO_YOLO2017_nomotor'
+# output_dir = '/media/linxu/MobilePan/2-Data/COCO_YOLO2017_nomotor'
+output_dir = '/media/linxu/MobilePan/2-Data/COCO_YOLO2017_nomotor1'
+
 # category_dict = {'person': 1, 'car': 0}  # 指定要抽取的类别和对应的类别索引
 category_dict = {'bicycle': 1, 'motorcycle': 0}  # 指定要抽取的类别和对应的类别索引
-
 extract_coco_data(coco_json_file, coco_img_dir, output_dir, category_dict)
