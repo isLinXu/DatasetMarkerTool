eba277bec5a5eb81a6d7f5339403605e3308d6b3
isLinXu <2267379130@qq.com>
2024-01-17 11:44:26 +0800

    [dataset][core][visual] update yolo_visualizer.py

--- dataset/core/visual/yolo_visualizer.py
+++ dataset/core/visual/yolo_visualizer.py
@@ -20,16 +20,16 @@ if class_names:
     class_names = [name.strip() for name in class_names.split(",")]
 
 # 生成颜色列表
-colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in range(len(class_names))]
+colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in
+          range(len(class_names) if class_names else 100)]
 
 if image_folder_path:
     image_files = os.listdir(image_folder_path)
     images = [file for file in image_files if file.endswith(".jpg") or file.endswith(".png")]
 
     # 选择图像文件
-    image_file = st.selectbox("选择图像文件:", images)
-    image_index = images.index(image_file)
-    st.number_input("图像文件索引:", min_value=0, max_value=len(images) - 1, step=1, value=image_index)
+    image_index = st.number_input("选择图像文件索引:", min_value=0, max_value=len(images) - 1, step=1, value=0)
+    image_file = st.selectbox("选择图像文件:", images, index=image_index)
 
     if image_file:
         # 显示图像
@@ -58,12 +58,14 @@ if image_folder_path:
                             x_max = int((x + w / 2) * width)
                             y_min = int((y - h / 2) * height)
                             y_max = int((y + h / 2) * height)
-                            color = colors[int(class_id)]
+                            color = colors[int(class_id)] if class_names else (0, 255, 0)
                             cv2.rectangle(image_cv, (x_min, y_min), (x_max, y_max), color, 2)
                             if class_names:
                                 class_name = class_names[int(class_id)]
-                                cv2.putText(image_cv, class_name, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                                            color, 2)
+                            else:
+                                class_name = str(int(class_id))
+                            cv2.putText(image_cv, class_name, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color,
+                                        2)
                 col1, col2 = st.columns(2)
                 col1.image(image, caption="原始图像", use_column_width=True)
                 col2.image(image_cv, caption="带边界框的图像", use_column_width=True)
