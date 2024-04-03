a5a688cd1237860edf6863cfd41b565c1f8d7f89
isLinXu <2267379130@qq.com>
2024-04-03 23:20:18 +0800

    [dataset][core][acc] update acc_val_img.py

--- dataset/core/acc/acc_val_img.py
+++ dataset/core/acc/acc_val_img.py
@@ -1,9 +1,9 @@
-
 import os
 import glob
 import shutil
 from tqdm import tqdm
 import cv2
+
 
 def bbox_iou(box1, box2):
     x1_min, y1_min, x1_max, y1_max = box1
@@ -14,13 +14,15 @@ def bbox_iou(box1, box2):
     intersection_x_max = min(x1_max, x2_max)
     intersection_y_max = min(y1_max, y2_max)
 
-    intersection_area = max(intersection_x_max - intersection_x_min, 0) * max(intersection_y_max - intersection_y_min, 0)
+    intersection_area = max(intersection_x_max - intersection_x_min, 0) * max(intersection_y_max - intersection_y_min,
+                                                                              0)
     box1_area = (x1_max - x1_min) * (y1_max - y1_min)
     box2_area = (x2_max - x2_min) * (y2_max - y2_min)
 
     union_area = box1_area + box2_area - intersection_area
 
     return intersection_area / union_area
+
 
 def parse_yolo_format(line, return_class_id=False):
     values = list(map(float, line.strip().split()))
@@ -48,23 +50,23 @@ def draw_bbox(image, bbox, class_id, color=(0, 255, 0), draw_type='pred', thickn
     draw_label = draw_type + ":" + str(class_id)
     cv2.putText(image, draw_label, (x_min, y_min - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness)
 
-
 def filter_fp_fn_images(prediction_path, ground_truth_path, image_path, root_path, iou_threshold=0.45):
     fp_images = []
     fn_images = []
     tp_images = []
     tn_images = []
 
-    fp_count = 0
-    fn_count = 0
-    tp_count = 0
-    tn_count = 0
+    # Initialize dictionaries to store counts for each class
+    fp_count = {}
+    fn_count = {}
+    tp_count = {}
+    tn_count = {}
 
-    # Get list of file names without extension
-    prediction_files = sorted([os.path.splitext(os.path.basename(x))[0] for x in glob.glob(os.path.join(prediction_path, '*.txt'))])
-    ground_truth_files = sorted([os.path.splitext(os.path.basename(x))[0] for x in glob.glob(os.path.join(ground_truth_path, '*.txt'))])
+    prediction_files = sorted(
+        [os.path.splitext(os.path.basename(x))[0] for x in glob.glob(os.path.join(prediction_path, '*.txt'))])
+    ground_truth_files = sorted(
+        [os.path.splitext(os.path.basename(x))[0] for x in glob.glob(os.path.join(ground_truth_path, '*.txt'))])
 
-    # Get list of common file names
     common_files = list(set(prediction_files) & set(ground_truth_files))
 
     fp_path = root_path + '/FP'
@@ -87,7 +89,7 @@ def filter_fp_fn_images(prediction_path, ground_truth_path, image_path, root_pat
     for file_name in tqdm(common_files, total=len(common_files)):
         pred_file = os.path.join(prediction_path, file_name + '.txt')
         gt_file = os.path.join(ground_truth_path, file_name + '.txt')
-        img_file = os.path.join(image_path, file_name + '.jpg')  # Assuming images are in .jpg format
+        img_file = os.path.join(image_path, file_name + '.jpg')
 
         with open(pred_file, 'r') as pf, open(gt_file, 'r') as gf:
             pred_lines = pf.readlines()
@@ -95,17 +97,15 @@ def filter_fp_fn_images(prediction_path, ground_truth_path, image_path, root_pat
 
             image = cv2.imread(img_file)
             if image is None:
-                # print(f"Warning: Unable to read image {img_file}. Skipping...")
                 continue
-            fp_detected = False
-            fn_detected = False
+
             for gt_line in gt_lines:
                 gt_bbox, gt_class_id = parse_yolo_format(gt_line, return_class_id=True)
-                draw_bbox(image, gt_bbox, gt_class_id, color=(0, 255, 0), draw_type='gt')  # Draw ground truth bbox in green
+                draw_bbox(image, gt_bbox, gt_class_id, color=(0, 255, 0), draw_type='gt')
 
             for pred_line in pred_lines:
                 pred_bbox, pred_class_id = parse_yolo_format(pred_line, return_class_id=True)
-                draw_bbox(image, pred_bbox, pred_class_id, color=(255, 0, 0), draw_type='pred')  # Draw predicted bbox in red
+                draw_bbox(image, pred_bbox, pred_class_id, color=(255, 0, 0), draw_type='pred')
 
                 max_iou = 0
                 for gt_line in gt_lines:
@@ -114,15 +114,29 @@ def filter_fp_fn_images(prediction_path, ground_truth_path, image_path, root_pat
                     max_iou = max(max_iou, iou)
 
                 if max_iou < iou_threshold:
-                    if not fp_detected:
-                        fp_count += 1
-                        fp_detected = True
+                    if pred_class_id not in fp_count:
+                        fp_count[pred_class_id] = 0
+                    fp_count[pred_class_id] += 1
+
+                    # Create class-specific folder if it doesn't exist
+                    class_fp_path = os.path.join(fp_path, str(pred_class_id))
+                    if not os.path.exists(class_fp_path):
+                        os.makedirs(class_fp_path)
+
                     fp_images.append(file_name)
-                    cv2.imwrite(os.path.join(fp_path, file_name + '.jpg'), image)
+                    cv2.imwrite(os.path.join(class_fp_path, file_name + '.jpg'), image)
                 else:
-                    tp_count += 1
+                    if pred_class_id not in tp_count:
+                        tp_count[pred_class_id] = 0
+                    tp_count[pred_class_id] += 1
+
+                    # Create class-specific folder if it doesn't exist
+                    class_tp_path = os.path.join(tp_path, str(pred_class_id))
+                    if not os.path.exists(class_tp_path):
+                        os.makedirs(class_tp_path)
+
                     tp_images.append(file_name)
-                    cv2.imwrite(os.path.join(tp_path, file_name + '.jpg'), image)
+                    cv2.imwrite(os.path.join(class_tp_path, file_name + '.jpg'), image)
 
             for gt_line in gt_lines:
                 gt_bbox, gt_class_id = parse_yolo_format(gt_line, return_class_id=True)
@@ -134,47 +148,71 @@ def filter_fp_fn_images(prediction_path, ground_truth_path, image_path, root_pat
                     max_iou = max(max_iou, iou)
 
                 if max_iou < iou_threshold:
-                    if not fn_detected:
-                        fn_count += 1
-                        fn_detected = True
+                    if gt_class_id not in fn_count:
+                        fn_count[gt_class_id] = 0
+                    fn_count[gt_class_id] += 1
+
+                    # Create class-specific folder if it doesn't exist
+                    class_fn_path = os.path.join(fn_path, str(gt_class_id))
+                    if not os.path.exists(class_fn_path):
+                        os.makedirs(class_fn_path)
+
                     fn_images.append(file_name)
-                    cv2.imwrite(os.path.join(fn_path, file_name + '.jpg'), image)
+                    cv2.imwrite(os.path.join(class_fn_path, file_name + '.jpg'), image)
                 else:
-                    tn_count += 1
+                    if gt_class_id not in tn_count:
+                        tn_count[gt_class_id] = 0
+                    tn_count[gt_class_id] += 1
+
+                    # Create class-specific folder if it doesn't exist
+                    class_tn_path = os.path.join(tn_path, str(gt_class_id))
+                    if not os.path.exists(class_tn_path):
+                        os.makedirs(class_tn_path)
+
                     tn_images.append(file_name)
-                    cv2.imwrite(os.path.join(tn_path, file_name + '.jpg'), image)
+                    cv2.imwrite(os.path.join(class_tn_path, file_name + '.jpg'), image)
+
     return fp_images, fn_images, tp_images, tn_images, fp_count, fn_count, tp_count, tn_count
 
-if __name__ == '__main__':
 
-    root_path = 'data/test_img/'
-    prediction_path = root_path + '/yolov6predictions'
-    ground_truth_path = 'datasets_v240402/labels/test'
-    image_path = 'datasets_v240402/images/test'
+root_path = 'datasets_v240402_04_01_large_infer'
+prediction_path = root_path + '/yolov6predictions'
+ground_truth_path = 'datasets_v240402/labels/test'
+image_path = 'datasets_v240402/images/test'
 
-    fp_images, fn_images, tp_images, tn_images, fp_count, fn_count, tp_count, tn_count = filter_fp_fn_images(prediction_path, ground_truth_path, image_path, root_path)
+fp_images, fn_images, tp_images, tn_images, fp_count, fn_count, tp_count, tn_count = filter_fp_fn_images(
+    prediction_path, ground_truth_path, image_path, root_path)
 
-    # 计算Recall、Precision和FAR
-    recall = tp_count / (tp_count + fn_count)
-    precision = tp_count / (tp_count + fp_count)
-    far = fp_count / (fp_count + tn_count)
+# Initialize dictionaries to store recall, precision and far for each class
+recall = {}
+precision = {}
+far = {}
 
-    print("Recall: {:.2f}".format(recall))
-    print("Precision: {:.2f}".format(precision))
-    print("FAR: {:.2f}".format(far))
+# Calculate recall, precision and far for each class
+num_classes = 2
+for class_id in range(num_classes):  # assuming num_classes is the number of classes
+    tp = tp_count.get(class_id, 0)
+    fn = fn_count.get(class_id, 0)
+    fp = fp_count.get(class_id, 0)
+    tn = tn_count.get(class_id, 0)
 
-    # print("False Positives:")
-    # for fp_image in fp_images:
-    #     print(fp_image)
+    # Print counts
+    print(f"Class {class_id}: TP = {tp}, FN = {fn}, FP = {fp}, TN = {tn}")
+    if tp + fn != 0:
+        recall[class_id] = tp / (tp + fn)
+    else:
+        recall[class_id] = None  # or some default value
 
-    # print("False Negatives:")
-    # for fn_image in fn_images:
-    #     print(fn_image)
+    if tp + fp != 0:
+        precision[class_id] = tp / (tp + fp)
+    else:
+        precision[class_id] = None  # or some default value
 
-    # print("True Positives:")
-    # for tp_image in tp_images:
-    #     print(tp_image)
+    if fp + tn != 0:
+        far[class_id] = fp / (fp + tn)
+    else:
+        far[class_id] = None  # or some default value
 
-    # print("True Negatives:")
-    # for tn_image in tn_images:
-    #     print(tn_image)
+# Print results
+for class_id in recall.keys():
+    print(f"Class {class_id}: Recall = {recall[class_id]}, Precision = {precision[class_id]}, FAR = {far[class_id]}")
