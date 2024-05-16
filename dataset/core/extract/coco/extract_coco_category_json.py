6bf2f753fce29bcbce3290141b6fa57981d9ff66
gatilin <2267379130@qq.com>
2024-05-16 17:21:49 +0800

    [datasets][core][extract] update extract_coco_category_json.py

--- dataset/core/extract/coco/extract_coco_category_json.py
+++ dataset/core/extract/coco/extract_coco_category_json.py
@@ -3,14 +3,12 @@ import os
 import shutil
 from tqdm import tqdm
 
-def extract_category(input_json_path, output_json_path, original_images_dir, new_images_dir, target_category_id):
+def extract_categories(input_json_path, output_json_path, original_images_dir, new_images_dir, target_category_ids):
     with open(input_json_path, 'r') as f:
         data = json.load(f)
 
-    # 初始化一个新的JSON数据结构
     new_data = {}
 
-    # 如果输入数据中存在相应字段，则将其添加到新数据结构中
     if "info" in data:
         new_data["info"] = data["info"]
     if "licenses" in data:
@@ -21,37 +19,28 @@ def extract_category(input_json_path, output_json_path, original_images_dir, new
     new_data["images"] = []
     new_data["annotations"] = []
 
-    # 保存已处理的图像ID，避免重复添加
     processed_image_ids = set()
 
-    # 遍历每个标注，使用tqdm显示进度
     for annotation in tqdm(data["annotations"], desc="Processing annotations"):
-        # 如果标注的类别与目标类别相匹配
-        if annotation["category_id"] == target_category_id:
-            # 将标注添加到新的JSON数据结构中
+        if annotation["category_id"] in target_category_ids:
             new_data["annotations"].append(annotation)
 
-            # 如果图像ID尚未处理，将对应的图像添加到新的JSON数据结构中，并复制图片文件
             image_id = annotation["image_id"]
             if image_id not in processed_image_ids:
                 image = next(image for image in data["images"] if image["id"] == image_id)
                 new_data["images"].append(image)
                 processed_image_ids.add(image_id)
 
-                # 复制图片文件
                 original_image_path = os.path.join(original_images_dir, image["file_name"])
                 new_image_path = os.path.join(new_images_dir, image["file_name"])
 
-                # 检查new_images_dir是否存在，如果不存在，则创建目录
                 if not os.path.exists(new_images_dir):
                     os.makedirs(new_images_dir)
                 if os.path.exists(original_image_path):
                     shutil.copy(original_image_path, new_image_path)
                 else:
                     continue
-                # shutil.copy(original_image_path, new_image_path)
 
-    # 将新的JSON数据结构保存到文件中
     with open(output_json_path, 'w') as f:
         json.dump(new_data, f)
 
@@ -60,6 +49,6 @@ input_json_path = "your_input_coco_json_file.json"
 output_json_path = "your_output_coco_json_file.json"
 original_images_dir = "path_to_your_original_images"
 new_images_dir = "path_to_your_new_images"
-target_category_id = 3  # 要提取的类别ID
+target_category_ids = [3, 5, 7]  # 要提取的类别ID列表
 
-extract_category(input_json_path, output_json_path, original_images_dir, new_images_dir, target_category_id)
+extract_categories(input_json_path, output_json_path, original_images_dir, new_images_dir, target_category_ids)
